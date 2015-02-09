92f8a4ee7dfd90fb3c1c7472d7ccc23d2abaceda
MartinHvidberg <Martin@Hvidberg.net>
2015-02-09 08:53:08 +0100

    a little more

--- DRrun.py
+++ DRrun.py
@@ -1,1 +1,12 @@
 # Running Data Reviewer, via ArcPy
+# This .py file is the Command line version.
+# It's very short, it simply calls the main executing module,
+# after setting up a few parameters.
+
+import DRrunner_execute
+
+if __name__ == "__main__":
+    DRrunner_execute.DRrun()
+
+# Music that accompanied the coding of this script:
+#   
---
+++ DRrunner_execute.py
@@ -0,0 +1,9 @@
+# Running Data Reviewer, via ArcPy
+# This .py file is the Executing module
+# It's supposed to be called from either the Commendline- or 
+# the ArcMapGUI-module'
+
+import arcpy
+
+def DRrun():
+    print "Hello world, from inside DRrun ..."
