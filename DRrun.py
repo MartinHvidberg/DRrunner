240cd24db25d4c4a86d825e651e1f858ee7f0c51
MartinHvidberg <Martin@Hvidberg.net>
2015-09-06 16:35:32 +0200

    SEQ1509061635 runs multi checks

--- DRrun.py
+++ DRrun.py
@@ -5,50 +5,65 @@
 
 import sys
 import os.path
+# import ECtimer XXX
 import DRrunner_execute
 import DRrunner_reporter
 
 if __name__ == "__main__":
     
-    # *** Setup parameters
+    print "DRrun : Init phase Begin ..."
+    
+    # *Setup parameters
     arcpy.env.overwriteOutput="true"
     
     # Set reviewer workspace
     reviewer_db = r"C:\Martin\Work\DR_runner_10_3_GR96.gdb"    
     # reviewer batch job file
-    str_filename_rbj = r"C:\Martin\Work\DRrunner_check.rbj" # XXX <------ Change this to list of file names    
+    lst_filename_rbj = [r"C:\Martin\Work\DRrunner_check1.rbj",r"C:\Martin\Work\DRrunner_check2.rbj"]    
     # production database - contains data to validate
     str_data_db = "C:\Martin\Work\NIScopy.gdb" # "C:/gisdata/Edit_Sample.sde"    
     
     # Check existance and validity of the above parameters
     if not os.path.exists(reviewer_db):
-        print "Can't find reviewer_db: "+reviewer_db
+        print "!!! Can't find reviewer_db: "+reviewer_db
         sys.exit(101)
-    if not os.path.exists(str_filename_rbj):
-        print "Can't find filename_rbj: "+str_filename_rbj
-        sys.exit(102)
+    for str_filename_rbj in lst_filename_rbj:
+        if not os.path.exists(str_filename_rbj):
+            print "!!! Can't find filename_rbj: "+str_filename_rbj
+            sys.exit(102)
     if not os.path.exists(str_data_db):
-        print "Can't find data_db: "+str_data_db
+        print "!!! Can't find data_db: "+str_data_db
         sys.exit(103)
+
+    print "DRrun : Init phase End ..." 
         
-    # *** Run the checks
+    # *Run the checks
      
     # Run
-    lst_run_ids = list()
-    lst_run_ids.append(DRrunner_execute.DRrun(reviewer_db, str_filename_rbj, str_data_db))
-    print "Done running - returning following ids:"
+    print "DRrun : Running phase Begin ..."
+        
+    lst_run_ids = list() # to cache the returning run IDs
+    for str_filename_rbj in lst_filename_rbj:
+        print "DRrun : call executer"
+        lst_run_ids.append(DRrunner_execute.DRrun(reviewer_db, str_filename_rbj, str_data_db))
+        print "DRrun : returned from executer"
+    print "DRrun : Returned the following ids:"
     for run_id in lst_run_ids:
-        print "    runID: "+str(run_id)
+        print "DRrun :    runID: "+str(run_id)
+        
+    print "DRrun : Running phase End ..."
     
-    # *** Optionally display the errors found ...
-     
+    # *Optionally display the errors found ...
+    print "DRrun : Reporting phase Begin ..."  
+       
     lst_error_reports = list()
     for id_i in lst_run_ids:
         dic_error_report = DRrunner_reporter.make_report_of_errors(reviewer_db, id_i)
-        #print "\nThis is the raw report:\n"+str(dic_error_report)
+        print "\n* This is the raw report:\n"+str(dic_error_report)
         DRrunner_reporter.show_report(dic_error_report)
     DRrunner_reporter.email_reports(lst_error_reports)
     
+    print "DRrun : Reporting phase End ..."
 
 # Music that accompanied the coding of this script:
 #    Doors - L.A.Woman
--- DRrunner_execute.py
+++ DRrunner_execute.py
@@ -9,7 +9,7 @@ import arcpy
 arcpy.CheckOutExtension("datareviewer")
 
 def DRrun(str_dr_db, str_filename_rbj, str_data_db):
-    print " > DRrunner_execute()", str_dr_db, str_filename_rbj, str_data_db
+    print "DRexe : DRrunner_execute()", str_dr_db, str_filename_rbj, str_data_db
     
     # Description: Executes a reviewer batch job
     # Requirements: Production Mapping extension
@@ -17,38 +17,37 @@ def DRrun(str_dr_db, str_filename_rbj, str_data_db):
     arcpy.env.overwriteOutput="true"
     
     # Delete, and then Create, the DR-session, to make sure it's clean.
-    print "Starting DR session"
-    str_session = "DRrunner" # A simple string wont cut it, I need to collect the full names ... XXX
+    print "DRexe : Starting DR session"
+    str_jobName = str_filename_rbj[:-4]
+    if str_jobName.rfind("\\")>0:
+        str_jobName = str_jobName[str_jobName.rfind("\\")+1:]    
+    str_session = "DRrun_"+str_jobName # A simple string won't cut it, I need to collect the full names ... XXX
     try:
         arcpy.DeleteReviewerSession_Reviewer(str_dr_db, str_session)
         # XXX Consider to delete all sessions older than e.g. 1 month.
-        # session="Session 1 : qc01"
-        # arcpy.DeleteReviewerSession_Reviewer(reviewer_gdb,session)
 
     except:
-        pass # print "   Session didn't exist - deleting nothing ..."
+        pass # print "DRexe : Session didn't exist - deleting nothing ..."
     drs_session = arcpy.CreateReviewerSession_Reviewer(str_dr_db, str_session)
     #Session = arcpy.CreateReviewerSession_Reviewer(Reviewer_workspace , "TestSession", "Session 1 : Session 1")
-    print "   DR session Create - Success : ", str(drs_session)
+    print "DRexe : DR session Create - Success : ", str(drs_session)
     
     # Execute Reviewer Batch Job function
-    print "Executing DR batch job: "+str_filename_rbj
+    print "DRexe : Executing DR batch job: "+str_filename_rbj
     res = arcpy.ExecuteReviewerBatchJob_Reviewer(str_dr_db, drs_session, str_filename_rbj, str_data_db)
-    print "   DR execute rbj - Success"
+    print "DRexe : DR execute rbj - Success"
     
     # get the output table
-    print "Show results 1 of "+str(len(res))+" (recordid, globalid, batchjobfile,runcontext, status, starttime, endtime) :"
+    print "DRexe : Show results 1 of "+str(len(res))+" (recordid, globalid, batchjobfile, runcontext, status, starttime, endtime) :"
     tbl = res.getOutput(0)
     # query the table
     for row in arcpy.da.SearchCursor(tbl,("*")):
-        for i in range(len(row)):
-            print "    "+str(i)+" : "+str(row[i])
+        #for i in range(len(row)):
+        #    print "DRexe :  > "+str(i)+" : "+str(row[i])
+        print "DRexe :    "+str(row)
         str_batch_run_id = row[1]
     
-    # Check in the Data Reviewer extension
-    arcpy.CheckInExtension("datareviewer")
-    
-    print " < DRrunner_execute() - Done... : ", str_batch_run_id
+    print "DRexe : DRrunner_execute() - Done... : ", str_batch_run_id
     return str_batch_run_id
 
     # ------ End def DRrun() -------------
