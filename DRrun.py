ad34e1edd3e29df24e085e254b9419ecefae084c
MartinHvidberg <Martin@Hvidberg.net>
2015-09-06 16:39:07 +0200

    150906,1638 nicer

--- DRrun.py
+++ DRrun.py
@@ -11,18 +11,17 @@ import DRrunner_reporter
 
 if __name__ == "__main__":
     
-    print "DRrun : Init phase Begin ..."
+    # *** Init
     
+    print "DRrun : Init phase Begin ..."    
     # *Setup parameters
-    arcpy.env.overwriteOutput="true"
-    
+    arcpy.env.overwriteOutput="true"    
     # Set reviewer workspace
     reviewer_db = r"C:\Martin\Work\DR_runner_10_3_GR96.gdb"    
     # reviewer batch job file
     lst_filename_rbj = [r"C:\Martin\Work\DRrunner_check1.rbj",r"C:\Martin\Work\DRrunner_check2.rbj"]    
     # production database - contains data to validate
-    str_data_db = "C:\Martin\Work\NIScopy.gdb" # "C:/gisdata/Edit_Sample.sde"    
-    
+    str_data_db = "C:\Martin\Work\NIScopy.gdb" # "C:/gisdata/Edit_Sample.sde"      
     # Check existance and validity of the above parameters
     if not os.path.exists(reviewer_db):
         print "!!! Can't find reviewer_db: "+reviewer_db
@@ -34,14 +33,11 @@ if __name__ == "__main__":
     if not os.path.exists(str_data_db):
         print "!!! Can't find data_db: "+str_data_db
         sys.exit(103)
-
     print "DRrun : Init phase End ..." 
         
-    # *Run the checks
+    # *** Run the checks
      
-    # Run
-    print "DRrun : Running phase Begin ..."
-        
+    print "DRrun : Running phase Begin ..."        
     lst_run_ids = list() # to cache the returning run IDs
     for str_filename_rbj in lst_filename_rbj:
         print "DRrun : call executer"
@@ -49,20 +45,18 @@ if __name__ == "__main__":
         print "DRrun : returned from executer"
     print "DRrun : Returned the following ids:"
     for run_id in lst_run_ids:
-        print "DRrun :    runID: "+str(run_id)
-        
+        print "DRrun :    runID: "+str(run_id)        
     print "DRrun : Running phase End ..."
     
-    # *Optionally display the errors found ...
-    print "DRrun : Reporting phase Begin ..."  
-       
+    # *** Optionally display the errors found ...
+    
+    print "DRrun : Reporting phase Begin ..."         
     lst_error_reports = list()
     for id_i in lst_run_ids:
         dic_error_report = DRrunner_reporter.make_report_of_errors(reviewer_db, id_i)
         print "\n* This is the raw report:\n"+str(dic_error_report)
         DRrunner_reporter.show_report(dic_error_report)
-    DRrunner_reporter.email_reports(lst_error_reports)
-    
+    DRrunner_reporter.email_reports(lst_error_reports)    
     print "DRrun : Reporting phase End ..."
 
 # Music that accompanied the coding of this script:
